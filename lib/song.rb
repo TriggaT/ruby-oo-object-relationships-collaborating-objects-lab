d0b41a2ca7bae200622a4f1d27e8eb7dd7ca6db7
Trayshawn <traytwebb14@yahoo.com>
2020-11-23 19:00:27 -0600

    commit tree 7a544b554b06fc951aba25d733fb63341059fb8e

---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+class Song
+
+    attr_accessor :name, :artist
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save 
+    end
+    
+    def save
+        @@all << self
+    end 
+
+    def self.all 
+        @@all
+    end 
+
+    def self.new_by_filename(filename)
+        song_name = filename.split(" - ")[1]
+        song_artist = filename.split(" - ")[0]
+        song = Song.new(song_name)
+        song.artist_name = song_artist
+        song
+    end
+
+    def artist_name=(name)
+        artist = Artist.find_or_create_by_name(name)
+        self.artist = artist
+        artist.add_song(self)
+    end
+
+end 
